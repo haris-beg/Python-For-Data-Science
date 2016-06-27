c7e4118189770a2c90af944e3706075aedbbd582
Haris Beg (r625361) <haris.beg@cambiahealth.com>
2016-06-27 11:13:06 -0700

    lists.py

---
+++ Basics/lists.py
@@ -0,0 +1,10 @@
+listOfNumbers = [1, 2, 3, 4, 5, 6]
+
+for number in listOfNumbers:
+    print number,
+    if (number % 2 == 0):
+        print "is Even"
+    else:
+        print "is Odd"
+        
+print "All done for today."
